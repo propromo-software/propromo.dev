99c603cbd719a9467287d6e03a4fc6aa0e83fbbb
Jonas <publik.froeller.jonas@gmx.at>
2024-04-29 16:45:13 +0200

    added port option

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const webserver = require('./app');
+require('dotenv').config();
 
 webserver
-  .listen(80)
+  .listen(process.env.PORT || 80)
   .then((socket) => console.log('Webserver started on http://localhost:80'))
   .catch((error) => console.log('Failed to start webserver on http://localhost:80'));
