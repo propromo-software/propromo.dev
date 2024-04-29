d9993a56af291b91da920900255322cf2b8594ea
Jonas <publik.froeller.jonas@gmx.at>
2024-04-29 12:12:44 +0200

    added `hyper-express`

--- src/app.ts
+++ src/app.ts
@@ -1,55 +1,7 @@
-/**
- * An output capable of recieving a string message for display.
- */
-export interface StringDisplay {
-  display(msg : string) : void;
-}
+import HyperExpress from 'hyper-express';
+export const webserver = new HyperExpress.Server();
 
-/**
- * Instance is an App controller. Automatically creates
- * model. Creates view if none given.
- */
-export class Controller {
-  private model : Model;
-  private view  : View ;
-
-  constructor (greeting: string, view?: View) {
-    this.model = new Model(greeting);
-    this.view  = (view  || new View());
-  }
-
-  public greet () : void {
-    this.view.display (this.model.getGreeting());
-  }
-}
-
-/**
- * Private class. Instance represents a greeting to the world.
- */
-class Model {
-  private greeting : string;
-
-  constructor (greeting : string) {
-    this.greeting = greeting;
-  }
-
-  public getGreeting() : string {
-    return this.greeting + ", world!";
-  }
-}
-
-/**
- * Instance is a message logger; outputs messages to console.
- */
-export class View implements StringDisplay {
-  public display (msg : string) : void {
-    console.log(msg);
-  }
-}
-
-/*
- * Factory function. Returns a default first app.
- */
-export function defaultGreeter(view? : StringDisplay) {
-  return new Controller("Hello", view);
-}
+// Create GET route to serve 'Hello World'
+webserver.get('/', (request, response) => {
+    response.send('Hello World');
+})
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 
-import * as App from "./app";
+import { webserver } from "./app";
 
-var greeter = new App.Controller("Whatup");
-
-greeter.greet();
+// Activate webserver by calling .listen(port, callback);
+webserver
+  .listen(80)
+  .then((socket) => console.log('Webserver started on port 80'))
+  .catch((error) => console.log('Failed to start webserver on port 80'));
